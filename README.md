b22ab6cb5e0d02d272aefd062781f5f4b9542491
Markus Westerlind <marwes91@gmail.com>
2016-01-15 15:09:34 +0100

    Add a better example to the README and the documentation root

--- README.md
+++ README.md
@@ -5,4 +5,36 @@ Rust library which implements a version of the [Any][] trait for types which con
 # WARNING
 This crate contains unsafe code which has not been properly vetted so this crate may not be safe to use.
 
+# Usage
+```
+#[macro_use]
+extern crate any_ref;
+use any_ref::AnyRef;
+
+struct MyWrapper(i32);
+any_ref!(MyWrapper);
+
+# #[allow(dead_code)]
+struct MyType<T, U>(T, U);
+any_ref!(MyType<T, U>);
+
+struct MyRef<'a, T: 'a, U>(&'a T, U);
+any_ref!(MyRef<'a, T, U>);
+
+fn main() {
+    let x = MyWrapper(1);
+    assert!((&x as &AnyRef).is::<MyWrapper>());
+    assert!(!(&x as &AnyRef).is::<i32>());
+    let r = MyRef(&x, ());
+    let ar = &r as &AnyRef;
+    assert!(ar.is::<MyRef<MyWrapper, ()>>());
+    assert!(ar.downcast_ref::<MyRef<MyWrapper, ()>>().is_some());
+
+    // Would be a compile error
+    // assert!((&r as &AnyRef).is::<MyRef<'static, MyWrapper, ()>>());
+}
+```
+
+[Documentation](https://marwes.github.io/any_ref/any_ref/index.html)
+
 [Any]:http://doc.rust-lang.org/std/any/trait.Any.html
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+//! # Usage
+//! ```
+//! #[macro_use]
+//! extern crate any_ref;
+//! use any_ref::AnyRef;
+//!
+//! struct MyWrapper(i32);
+//! any_ref!(MyWrapper);
+//!
+//! # #[allow(dead_code)]
+//! struct MyType<T, U>(T, U);
+//! any_ref!(MyType<T, U>);
+//!
+//! struct MyRef<'a, T: 'a, U>(&'a T, U);
+//! any_ref!(MyRef<'a, T, U>);
+//!
+//! fn main() {
+//!     let x = MyWrapper(1);
+//!     assert!((&x as &AnyRef).is::<MyWrapper>());
+//!     assert!(!(&x as &AnyRef).is::<i32>());
+//!     let r = MyRef(&x, ());
+//!     let ar = &r as &AnyRef;
+//!     assert!(ar.is::<MyRef<MyWrapper, ()>>());
+//!     assert!(ar.downcast_ref::<MyRef<MyWrapper, ()>>().is_some());
+//!
+//!     // Would be a compile error
+//!     // assert!((&r as &AnyRef).is::<MyRef<'static, MyWrapper, ()>>());
+//! }
+//! ```
 use std::mem;
 use std::any::{Any, TypeId};
 
@@ -20,23 +49,6 @@ pub fn type_id<'a, T>() -> TypeId
 /// `Any` for types containing borrows.
 ///
 /// Safe(?) if implemented using the provided any_ref macro
-///
-/// ```
-/// #[macro_use]
-/// extern crate any_ref;
-///
-/// struct MyWrapper(i32);
-/// any_ref!(MyWrapper);
-///
-/// struct MyType<T, U>(T, U);
-/// any_ref!(MyType<T, U>);
-///
-/// struct MyRef<'a, T: 'a, U>(&'a T, U);
-/// any_ref!(MyRef<'a, T, U>);
-///
-/// # fn main() {
-/// # }
-/// ```
 pub unsafe trait AnyRef<'a> {
     /// Converts `&self` to `&Any`
     ///
